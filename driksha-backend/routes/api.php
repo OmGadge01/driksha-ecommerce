c889c1331a79634db65b6d38812bd9936d0d5194
OmGadge01 <omgadge19434@gmail.com>
2026-05-19 16:33:09 +0530

    COrrected authentication routes

--- driksha-backend/routes/api.php
+++ driksha-backend/routes/api.php
@@ -3,16 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\ProductController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
+
     Route::get('/profile', [AuthController::class, 'profile']);
     Route::post('/logout', [AuthController::class, 'logout']);
+
     Route::prefix('admin')->group(function () {
-    Route::apiResource('products', ProductController::class);
+        Route::apiResource('products', ProductController::class);
+    });
 
 });
