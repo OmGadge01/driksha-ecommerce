2653a023f5a302c9772afcdea8db463887a42027
OmGadge01 <omgadge19434@gmail.com>
2026-07-05 01:56:44 +0530

    Update AuthRoutes.jsx

--- frontend/src/routes/AuthRoutes.jsx
+++ frontend/src/routes/AuthRoutes.jsx
@@ -2,7 +2,7 @@ import { Route } from "react-router-dom";
 
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
-import Home from "../pages/customer/Home";
+
 
 const AuthRoutes = () => {
   return (
